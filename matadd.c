2f34b2947185a8a181c16acce9d92dd2961b59b3
Jon Craton <jncraton@gmail.com>
2021-04-08 16:05:08 -0400

    Add matrix multiplication

--- matadd.c
+++ matadd.c
@@ -3,9 +3,9 @@
 #include <stdio.h>
 #include <malloc.h>
 
-#define SIZE 16384
+#define SIZE 1024
 
-void add(int a[SIZE][SIZE], int b[SIZE][SIZE]) {
+void add(float a[SIZE][SIZE], float b[SIZE][SIZE]) {
   // Adds matrix `b` to matrix `a` replacing the contents of matrix `a`
 
   for (int i = 0; i < SIZE; i++) {
@@ -15,16 +15,31 @@ void add(int a[SIZE][SIZE], int b[SIZE][SIZE]) {
   }
 }
 
+void mul(float a[SIZE][SIZE], float b[SIZE][SIZE], float c[SIZE][SIZE]) {
+  // Adds matrix `b` to matrix `a` replacing the contents of matrix `a`
+
+  for (int i = 0; i < SIZE; i++) {
+    for (int j = 0; j < SIZE; j++) {
+      c[j][i] = 0;
+
+      for (int k = 0; k < SIZE; k++) {
+        c[j][i] += a[j][k] + b[k][i];
+      }
+    }
+  }
+}
+
 int main() {
-  static int a[SIZE][SIZE];
-  static int b[SIZE][SIZE];
+  static float a[SIZE][SIZE];
+  static float b[SIZE][SIZE];
+  static float c[SIZE][SIZE];
 
   printf("Initializing matrices...\n");
 
   for (int i = 0; i < SIZE; i++) {
     for (int j = 0; j < SIZE; j++) {
       a[i][j] = i+2*j;
-      b[i][j] = i-j; 
+      b[i][j] = i-j;
     }
   }
 
@@ -41,6 +56,13 @@ int main() {
       assert(a[i][j] == 2*i+j);
     }
   }
+
+  printf("Multiplying matrices...\n");
+
+  start = clock();
+  mul(a, b, c);
+  printf("Multiplication completed in %ld ms\n", 1000 * (clock() - start) / CLOCKS_PER_SEC);
+
 
   printf("Success!\n");
   return 0;
